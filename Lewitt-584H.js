f4d27d05624b9881a17887ba8698a9d6b1c626c6
hbar4 <35719981+hbar4@users.noreply.github.com>
2018-02-04 13:45:09 -0800

    Create Lewitt-584H.js

---
+++ Lewitt-584H.js
@@ -0,0 +1,16 @@
+function setup() {
+createCanvas (300,150);
+}
+function draw() {
+colorMode(RGB);
+background(255);
+noStroke();
+fill(185,121,198);
+rect(0,0,150,75);
+  fill(213,211,133);
+  rect(0,75,150,75);
+  fill(102,204,0);
+  rect(150,0,150,75);
+  fill(255,128,0);
+  rect(150,75,150,75);
+}
